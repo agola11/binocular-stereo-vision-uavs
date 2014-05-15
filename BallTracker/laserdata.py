7eafaf9e09b1a6ed8ea184ab5dcff3e8c9399e9c
root <root@beaglebone.(none)>
2014-05-15 02:32:40 +0000

    turns on laser light permanently

--- BallTracker/laserdata.py
+++ BallTracker/laserdata.py
@@ -8,6 +8,9 @@ ser = serial.Serial(port = "/dev/ttyO1",baudrate=115200,timeout=1)
 ser.close()
 ser.open()
 
+#Turn on laser light
+ser.write("*100515#")
+#Take a measurement
 ser.write("*00004#")
 for x in range(0,3):
     s= ser.readline()
