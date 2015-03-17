531e9dcca7a946930e61fb3d290ba13e478fe1a3
root <root@beaglebone.localdomain>
2015-03-17 06:34:25 +0000

    finished initial draft of port with timing code

--- BallTracker/test/ball_tracker.cpp
+++ BallTracker/test/ball_tracker.cpp
@@ -5,6 +5,7 @@
 
 #include<iostream>
 #include<opencv2/opencv.hpp>
+#include<sys/time.h>
 using namespace std;
 using namespace cv;
 
@@ -12,27 +13,56 @@ int main()
 {
     VideoCapture capture(0);
     if(!capture.isOpened()){
-	    cout << "Failed to connect to the camera." << endl;
+        cout << "Failed to connect to the camera." << endl;
     }
-    Mat frame, hsv, mask;
-    vector<Vec3f> circles;
-    capture >> frame;
-    if(frame.empty()){
-		cout << "Failed to capture an image" << endl;
-		return -1;
-    }
-    cvtColor(frame, hsv, CV_BGR2HSV);
 
-    //Normal masking algorithm
-    Vec3i lower_blue(175, 68, 126);
-    Vec3i upper_blue(185, 255, 247);
-    inRange(hsv,lower_blue,upper_blue,mask);
+    for(int i = 0; i < 20; i++){
+        struct timeval start, image_aquired, end;
+        struct timezone tz;
+        double get, process;
+        gettimeofday(&start,&tz);
 
-    //other stuff here
+        //get a frame
+        Mat frame, hsv, mask;
+        capture >> frame;
+        if(frame.empty()){
+            cout << "Failed to capture an image" << endl;	
+            continue;
+        }
+        gettimeofday(&image_aquired,&tz);
+
+        //HSV conversion
+        cvtColor(frame, hsv, CV_BGR2HSV);
     
-    //blur image
-    GaussianBlur(mask, mask, Size(15,15), 0);
-
-    //detect circles
+        //Normal masking algorithm
+        Vec3i lower_blue(175, 68, 126);
+        Vec3i upper_blue(185, 255, 247);
+        inRange(hsv,lower_blue,upper_blue,mask);
+        
+        //Morphological operations
+        Mat kernel_close = getStructuringElement(MORPH_RECT,Size(21,21));
+        Mat kernel_open = getStructuringElement(MORPH_RECT,Size(11,11));
+        morphologyEx(mask,mask,MORPH_CLOSE,kernel_close);
+        morphologyEx(mask,mask,MORPH_OPEN,kernel_open);
+    
+        //Blur image
+        GaussianBlur(mask, mask, Size(15,15), 0);
+        
+        //Detect circles 
+        vector<Vec3f> circles;
+        HoughCircles(mask,circles,CV_HOUGH_GRADIENT,1,1600,50,20,0,0);
+        
+        gettimeofday(&end,&tz);
+        long seconds, useconds;
+        seconds = image_aquired.tv_sec-start.tv_sec;
+        useconds = image_aquired.tv_usec-start.tv_usec;
+        get = seconds * 1000 + useconds/1000.0;
+        seconds = end.tv_sec - image_aquired.tv_sec;
+        useconds = end.tv_usec - image_aquired.tv_usec; 
+        process = seconds * 1000 + useconds/1000.0;
+        cout << get << " ";
+        cout << process;
+        cout <<  endl;
+    }
     return 0;
 }
