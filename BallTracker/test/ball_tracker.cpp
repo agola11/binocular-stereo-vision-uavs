76be8c5c28dafcdd45c369ea7871ae4b503de76e
root <root@beaglebone.localdomain>
2015-03-17 02:01:25 +0000

    more porting work

--- BallTracker/test/ball_tracker.cpp
+++ BallTracker/test/ball_tracker.cpp
@@ -1,12 +1,6 @@
-/* boneCV.cpp
- *
- * Copyright Derek Molloy, School of Electronic Engineering, Dublin City University
- * www.derekmolloy.ie
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that source code redistributions retain this notice.
- *
- * This software is provided AS IS and it comes with no warranties of any type. 
+/*
+ * Joseph Bolling and Ankush Gola
+ * C++ port of ball tracking module
  */
 
 #include<iostream>
@@ -17,20 +11,28 @@ using namespace cv;
 int main()
 {
     VideoCapture capture(0);
-    capture.set(CV_CAP_PROP_FRAME_WIDTH,1920);
-    capture.set(CV_CAP_PROP_FRAME_HEIGHT,1080);
     if(!capture.isOpened()){
 	    cout << "Failed to connect to the camera." << endl;
     }
-    Mat frame, edges;
+    Mat frame, hsv, mask;
+    vector<Vec3f> circles;
     capture >> frame;
     if(frame.empty()){
 		cout << "Failed to capture an image" << endl;
 		return -1;
     }
-    cvtColor(frame, edges, CV_BGR2GRAY);
-    Canny(edges, edges, 0, 30, 3);
-    imwrite("edges.png", edges);
-    imwrite("capture.png", frame);
+    cvtColor(frame, hsv, CV_BGR2HSV);
+
+    //Normal masking algorithm
+    Vec3i lower_blue(175, 68, 126);
+    Vec3i upper_blue(185, 255, 247);
+    inRange(hsv,lower_blue,upper_blue,mask);
+
+    //other stuff here
+    
+    //blur image
+    GaussianBlur(mask, mask, Size(15,15), 0);
+
+    //detect circles
     return 0;
 }
