0be2eebf7462a61ef1a09df44a5361408fc4dcc2
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-a-20809.princeton.edu>
2015-04-11 03:41:16 -0400

    added some FSM logic to copter_client - not fully done

--- copter_control/sock_server.py
+++ copter_control/sock_server.py
@@ -1,33 +1,119 @@
+'''
+sock_server.py:
+	Copter controller FSM for formation flying.
+
+Author:
+	Ankush Gola
+'''
 import socket, time
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 6060))
-serversocket.listen(2) # listen for 2 connections
+PORT = 6060
+BUF_SIZE = 4096
 
-def recv_messages((c1, a1), (c2, a2)):
-	msg1 = c1.recv(1024)
-	msg2 = c2.recv(1024)
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('localhost', PORT))
+ss.listen(2) # listen for 2 connections
 
+c1, c2 = None, None
+a1, a2 = None, None
+state = 0
+
+def error(ss, msg):
+	"""
+	for debugging
+	"""
+	print('ERR: ' + msg)
+	ss.close()
+
+def print_msgs(msg1, msg2):
+	"""
+	for debugging
+	"""
+	global a1, a2
 	print a1, ' >> ', msg1
 	print a2, ' >> ', msg2
 
-c1, a1 = serversocket.accept()
-c2, a2 = serversocket.accept()
-print c1, a1
-print c2, a2
+def start():
+	"""
+	Handle the starting state
+	establish a connection between the two clients
+	"""
+	global c1, c2
+	global a1, a2
+	global state
+
+	c1, a1 = ss.accept()
+	c2, a2 = ss.accept()
+	state += 1
+
+def arm_first():
+	"""
+	Handle the single arm state
+	arm the first copter
+	"""
+	global c1, c2
+	global a1, a2
+	global BUF_SIZE
+	global state
+
+	msg1 = c1.recv(BUF_SIZE)
+	msg2 = c2.recv(BUF_SIZE)
+	print_msgs(msg1, msg2) # wait for connected messages
+
+	if msg1 != 'Connected' or msg2 != 'Connected':
+		state = 9 # exit failure
+	else:
+		new_msg = 'ARM'
+		c1.send(new_msg)
+		state += 1
+
+def takeoff_first(c):
+	# wait for armed messages
+	pass
+
+def goto_first(c):
+	pass
+
+def arm_second(c):
+	pass
+
+def takeoff_second(c):
+	pass
+
+def goto_second(c):
+	pass
+
+def formation(c):
+	pass
+
+def exit_success(c):
+	pass
+
+def exit_failure(c):
+	pass
+
+states = {0 : start, 1 : arm_first, 2 : takeoff_first, 3 : goto_first, 4 : arm_second, 5 : takeoff_second, 6 : goto_second, 7 : formation, 8 : exit_success, 9 : exit_failure}
+
+# propogate FSM
+while state < len(states):
+	state = states[state]()
+
+
+c1, a1 = ss.accept()
+c2, a2 = ss.accept()
 
 recv_messages((c1, a1), (c2, a2))
 
-new_msg = 'OKAY TO ARM'
+new_msg = 'ARM'
 c1.send(new_msg)
 c2.send(new_msg)
 
 recv_messages((c1, a1), (c2, a2))
 
-new_msg = 'OKAY TO TAKEOFF'
+new_msg = 'TAKEOFF'
 c1.send(new_msg)
 c2.send(new_msg)
 
 recv_messages((c1, a1), (c2, a2))
 
-serversocket.close()
+ss.close()
