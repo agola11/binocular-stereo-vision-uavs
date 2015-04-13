16e802ab8d14b28d6d53248b09ff1b91feac32d4
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-a-20809.princeton.edu>
2015-04-13 14:12:41 -0400

    fixed some bugs

--- copter_control/sock_server.py
+++ copter_control/sock_server.py
@@ -5,7 +5,7 @@ sock_server.py:
 Author:
 	Ankush Gola
 '''
-import socket, time
+import socket, time, json
 
 PORT = 6060
 BUF_SIZE = 4096
@@ -23,7 +23,6 @@ def error(ss, msg):
 	for debugging
 	"""
 	print('ERR: ' + msg)
-	ss.close()
 
 def print_msgs(msg1, msg2):
 	"""
@@ -67,53 +66,112 @@ def arm_first():
 		c1.send(new_msg)
 		state += 1
 
-def takeoff_first(c):
-	# wait for armed messages
+def takeoff_first():
+	"""
+	Handle the single takeoff state.
+	takeoff the first copter.
+	"""
+	global c1
+	global a1
+	global BUF_SIZE
+	global state
+
+	msg = c1.recv(BUF_SIZE) # wait for the armed message
+	print a1, ' >> ', msg
+	if msg != 'Armed'
+		state = 9 # exit failure
+	else:
+		new_msg = 'TAKEOFF'
+		c1.send(new_msg)
+		state += 1
+	
+
+def goto_first():
+	"""
+	Handle the first goto waypoint state.
+	"""
+	global c1
+	global a1
+	global BUF_SIZE
+	global state
+
+	msg = c1.recv(BUF_SIZE) # wait for taken off message
+	print a1, ' >> ', msg1
+	if msg != 'Taken Off'
+		state = 9 # exit failure
+	else:
+		new_msg = 'GOTO'
+		c1.send(new_msg)
+		state += 1
+
+def arm_second():
+	"""
+	Handle the second armed state
+	arm the second copter
+	"""
+	global c1, c2
+	global a1, a2
+	global BUF_SIZE
+	global state
+
+	msg1 = c1.recv(BUF_SIZE) # wait for the arrival message from first copter
+	print a1, ' >> ', msg1
+	if msg1 != 'Arrived':
+		state = 9 # exit failure
+	else:
+		new_msg = 'TAKEOFF'
+		c2.send(new_msg)
+		state += 1
+
+def takeoff_second():
+	"""
+	Handle the single takeoff state.
+	takeoff the first copter.
+	"""
+	global c2
+	global a2
+	global BUF_SIZE
+	global state
+
+	msg = c2.recv(BUF_SIZE) # wait for the armed message
+	print a2, ' >> ', msg
+	if msg != 'Armed'
+		state = 9 # exit failure
+	else:
+		new_msg = 'TAKEOFF'
+		c2.send(new_msg)
+		state += 1
+
+def goto_second():
+	global c2
+	global a2
+	global BUF_SIZE
+	global state
+
+	msg = c2.recv(BUF_SIZE) # wait for the armed message
+	print a2, ' >> ', msg
+	if msg != 'Armed'
+		state = 9 # exit failure
+	else:
+		new_msg = 'TAKEOFF'
+		c2.send(new_msg)
+		state += 1
+
+def formation():
 	pass
 
-def goto_first(c):
-	pass
+def exit_success():
+	global state
+	state += 1 # increment state to 10
 
-def arm_second(c):
-	pass
-
-def takeoff_second(c):
-	pass
-
-def goto_second(c):
-	pass
-
-def formation(c):
-	pass
-
-def exit_success(c):
-	pass
-
-def exit_failure(c):
-	pass
+def exit_failure():
+	global state
+	state += 1 # increment state to 10
 
 states = {0 : start, 1 : arm_first, 2 : takeoff_first, 3 : goto_first, 4 : arm_second, 5 : takeoff_second, 6 : goto_second, 7 : formation, 8 : exit_success, 9 : exit_failure}
 
 # propogate FSM
 while state < len(states):
 	state = states[state]()
-
-
-c1, a1 = ss.accept()
-c2, a2 = ss.accept()
-
-recv_messages((c1, a1), (c2, a2))
-
-new_msg = 'ARM'
-c1.send(new_msg)
-c2.send(new_msg)
-
-recv_messages((c1, a1), (c2, a2))
-
-new_msg = 'TAKEOFF'
-c1.send(new_msg)
-c2.send(new_msg)
-
-recv_messages((c1, a1), (c2, a2))
 
 ss.close()
