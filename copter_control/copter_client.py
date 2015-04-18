3a9d4827325cd4a6523e2df6c444d51e7fad903b
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-a-20809.princeton.edu>
2015-04-18 14:08:18 -0400

    got simulated FSM to work

--- copter_control/copter_client.py
+++ copter_control/copter_client.py
@@ -1,37 +1,158 @@
-import time, socket
+'''
+sock_client.py:
+	Simulated Quadcopter Client
+
+Author:
+	Ankush Gola
+'''
+
+import time, socket, json
 from copter_control import CopterControl
+
+def print_srv(msg):
+	"""
+	Print message from the server
+	"""
+	print "Server:", " >> ", msg
+
+def error(msg):
+	"""
+	for debugging
+	"""
+	print('ERR: ' + msg)
+
+def connect():
+	"""
+	Handle the connect state.
+	Establish a connection with the server
+	"""
+
+	print "In connection state"
+
+	global cs
+	global state
+
+	cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	cs.connect(('localhost', PORT))
+	cs.send('Connected')
+	state += 1
+
+def arm():
+	"""
+	Handle the arm state.
+	Arm the copter.
+	"""
+
+	print "In arm state"
+
+	global cs, cc
+	global state
+
+	msg = cs.recv(BUF_SIZE) 
+
+	if msg != 'ARM':
+		error(msg)
+		state = 6 # exit failure
+	else:
+		cc.arm()
+		cc.set_mode('GUIDED')
+
+		to = time.time() + TIMEOUT
+		while not cc.is_armed() and cc.get_mode_name() != 'GUIDED':
+			if time.time() > to:
+				cs.send('TIMEOUT')
+				state = 6
+				return
+			time.sleep(0.1)
+
+		cs.send('Armed')
+		state += 1
+
+def takeoff():
+	"""
+	Handle the takeoff state.
+	Make the copter takeoff
+	"""
+
+	print "In the takeoff state"
+
+	global cs, cc
+	global state
+
+	msg = cs.recv(BUF_SIZE) # wait for server message
+	
+	if msg != 'TAKEOFF':
+		error(msg)
+		state = 6 # exit failure
+	else:
+		cc.takeoff(15)
+		# cc.set_mode('GUIDED')
+		# cc.takeoff(json.loads(msg)['alt'])
+		# Insert while loop with timeout here
+		time.sleep(4)
+		cs.send('Taken Off')
+		state += 1
+
+def goto_init():
+	"""
+	Handle the goto_init state.
+	Make the copter go to the first location.
+	"""
+
+	print "In the goto_init state"
+
+	global cs
+	global state
+
+	msg = cs.recv(BUF_SIZE) # wait for server message
+	
+	if msg != 'GOTO':
+		error(msg)
+		state = 6 # exit failure
+	else:
+		# cc.arm()
+		# cc.set_mode('GUIDED')
+		# cc.takeoff(json.loads(msg)['alt'])
+		# Insert while loop with timeout here
+		time.sleep(4)
+		cs.send('Arrived')
+		state += 1
+
+def formation():
+	print "Formation"
+	global state
+
+	msg = cs.recv(BUF_SIZE)
+	state += 1
+
+def exit_success():
+	print "EXIT SUCCESS"
+
+	global state
+	state += 2
+
+def exit_failure():
+	global cs
+	global state
+
+	print "EXIT FAILURE"
+	state += 1
+
 
 PORT = 6060
 BUF_SIZE = 4096
+TIMEOUT = 30 # timeout in seconds
+state = 0 # initial state
+cs = None
+
+states = {0 : connect, 1 : arm, 2 : takeoff, 3 : goto_init, 4 : formation, 5 : exit_success, 6 : exit_failure}
 
 # DroneAPI APIConnection
 api = local_connect()
 cc = CopterControl(api)
 
-cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cs.connect(('localhost', PORT))
-cs.send('connected')
-
-msg = cs.recv(BUF_SIZE)
-print msg
-
-# Arm
-print 'ATTEMPTING TO ARM'
-
-cc.arm()
-cc.set_mode('GUIDED')
-
-while not cc.is_armed() and cc.get_mode_name() != 'GUIDED':
-	time.sleep(0.5)
-
-cs.send('ARMED')
-
-msg = cs.recv(BUF_SIZE)
-print msg
-
-# Takeoff
-cc.takeoff(60)
-
-cs.send('TAKEN OFF')
+# propogate FSM
+while state < len(states):
+	states[state]()
 
 cs.close()
--- copter_control/sock_server.py
+++ copter_control/sock_server.py
@@ -7,18 +7,7 @@ Author:
 '''
 import socket, time, json
 
-PORT = 6060
-BUF_SIZE = 4096
-
-ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ss.bind(('localhost', PORT))
-ss.listen(2) # listen for 2 connections
-
-c1, c2 = None, None
-a1, a2 = None, None
-state = 0
-
-def error(ss, msg):
+def error(msg):
 	"""
 	for debugging
 	"""
@@ -60,6 +49,8 @@ def arm_first():
 	print_msgs(msg1, msg2) # wait for connected messages
 
 	if msg1 != 'Connected' or msg2 != 'Connected':
+		error(msg1)
+		error(msg2)
 		state = 9 # exit failure
 	else:
 		new_msg = 'ARM'
@@ -78,14 +69,16 @@ def takeoff_first():
 
 	msg = c1.recv(BUF_SIZE) # wait for the armed message
 	print a1, ' >> ', msg
-	if msg != 'Armed'
+	if msg != 'Armed':
+		error(msg)
 		state = 9 # exit failure
 	else:
-		new_msg = 'TAKEOFF'
-		c1.send(new_msg)
+		new_msg = {}
+		new_msg['msg'] = 'TAKEOFF'
+		new_msg['arg1'] = init1[2]
+		c1.send(json.dumps(new_msg))
 		state += 1
 	
-
 def goto_first():
 	"""
 	Handle the first goto waypoint state.
@@ -96,12 +89,15 @@ def goto_first():
 	global state
 
 	msg = c1.recv(BUF_SIZE) # wait for taken off message
-	print a1, ' >> ', msg1
-	if msg != 'Taken Off'
+	print a1, ' >> ', msg
+	if msg != 'Taken Off':
+		error(msg)
 		state = 9 # exit failure
 	else:
-		new_msg = 'GOTO'
-		c1.send(new_msg)
+		new_msg = {}
+		new_msg['msg'] = 'GOTO'
+		new_msg['arg1'] = init1
+		c1.send(json.dumps(new_msg))
 		state += 1
 
 def arm_second():
@@ -117,9 +113,10 @@ def arm_second():
 	msg1 = c1.recv(BUF_SIZE) # wait for the arrival message from first copter
 	print a1, ' >> ', msg1
 	if msg1 != 'Arrived':
+		error(msg1)
 		state = 9 # exit failure
 	else:
-		new_msg = 'TAKEOFF'
+		new_msg = 'ARM'
 		c2.send(new_msg)
 		state += 1
 
@@ -135,43 +132,130 @@ def takeoff_second():
 
 	msg = c2.recv(BUF_SIZE) # wait for the armed message
 	print a2, ' >> ', msg
-	if msg != 'Armed'
+	if msg != 'Armed':
+		error(msg)
 		state = 9 # exit failure
 	else:
-		new_msg = 'TAKEOFF'
-		c2.send(new_msg)
+		new_msg = {}
+		new_msg['msg'] = 'TAKEOFF'
+		new_msg['arg1'] = init2[2]
+		c2.send(json.dumps(new_msg))
 		state += 1
 
 def goto_second():
+	"""
+	Handle the goto second state.
+	"""
 	global c2
 	global a2
 	global BUF_SIZE
 	global state
 
-	msg = c2.recv(BUF_SIZE) # wait for the armed message
+	msg = c2.recv(BUF_SIZE) # wait for the taken off message
 	print a2, ' >> ', msg
-	if msg != 'Armed'
+	if msg != 'Taken Off':
+		error(msg)
 		state = 9 # exit failure
 	else:
-		new_msg = 'TAKEOFF'
-		c2.send(new_msg)
+		new_msg = {}
+		new_msg['msg'] = 'GOTO'
+		new_msg['arg1'] = init2
+		c2.send(json.dumps(new_msg))
 		state += 1
 
 def formation():
-	pass
+	"""
+	Handle the formation state.
+	wait for messages at each stage of the path step.
+	"""
+	global c1, c2
+	global a1, a2
+	global BUF_SIZE
+	global state
+
+	msg = c2.recv(BUF_SIZE) # wait for the arrived message
+	print a2, ' >> ', msg
+	if msg != 'Arrived':
+		error(msg)
+		state = 9 # exit failure
+	else:
+		msg1, msg2 = {}, {}
+		msg1['msg'] = 'FORMATION'
+		msg2['msg'] = 'FORMATION'
+		
+		msg1['arg1'] = init1
+		msg1['arg2'] = end1
+		msg1['arg3'] = step
+
+		msg2['arg1'] = init2
+		msg2['arg2'] = end2
+		msg2['arg3'] = step
+
+		c1.send(json.dumps(msg1))
+		c2.send(json.dumps(msg2))
+		while True:
+			msg1 = c1.recv(BUF_SIZE)
+			msg2 = c2.recv(BUF_SIZE)
+			if msg1 == 'DONE' and msg2 == 'DONE':
+				break
+			elif msg1 != 'Arrived' or msg2 != 'Arrived':
+				error(msg1)
+				error(msg2)
+				state = 9
+				return
+			else:
+				new_msg = 'GO'
+				c1.send(new_msg)
+				c2.send(new_msg)
+		
+		state += 1
 
 def exit_success():
+	"""
+	Handle the exit success state.
+	"""
 	global state
-	state += 1 # increment state to 10
+	print "EXIT SUCCESS"
+	state += 2 # increment state to 10
 
 def exit_failure():
+	"""
+	Handle the exit failure state.
+	"""
 	global state
+	global c1, c2
+	global a1, a2
+
+	# Tell clients to close connections
+	c1.send("CLOSE")
+	c2.send("CLOSE")
+
+	print "EXIT FAILURE"
 	state += 1 # increment state to 10
 
 states = {0 : start, 1 : arm_first, 2 : takeoff_first, 3 : goto_first, 4 : arm_second, 5 : takeoff_second, 6 : goto_second, 7 : formation, 8 : exit_success, 9 : exit_failure}
 
+PORT = 6060
+BUF_SIZE = 4096
+
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('localhost', PORT))
+ss.listen(2) # listen for 2 connections
+
+c1, c2 = None, None
+a1, a2 = None, None
+state = 0
+
+init1 = (40.345851, -74.650123, 15) # 40 left
+end1 = (40.345969, -74.650181, 30) # 20 left
+
+init2 = (40.345654, -74.650001, 15) # 40 right
+end2 = (40.345520, -74.649927, 30) # 20 left
+
+step = 5
+
 # propogate FSM
 while state < len(states):
-	state = states[state]()
+	states[state]()
 
 ss.close()
