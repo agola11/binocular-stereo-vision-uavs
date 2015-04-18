42f2b6ac612624387538425beb6794458cf8d755
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-a-20809.princeton.edu>
2015-04-18 15:34:26 -0400

    added logic for actual copter control

--- copter_control/copter_client.py
+++ copter_control/copter_client.py
@@ -1,12 +1,13 @@
 '''
 sock_client.py:
-	Simulated Quadcopter Client
+	Simulated Copter Client
 
 Author:
 	Ankush Gola
 '''
 
 import time, socket, json
+import numpy as np
 from copter_control import CopterControl
 
 def print_srv(msg):
@@ -56,7 +57,7 @@ def arm():
 	else:
 		cc.arm()
 		cc.set_mode('GUIDED')
-
+		
 		to = time.time() + TIMEOUT
 		while not cc.is_armed() and cc.get_mode_name() != 'GUIDED':
 			if time.time() > to:
@@ -64,9 +65,12 @@ def arm():
 				state = 6
 				return
 			time.sleep(0.1)
-
+		
 		cs.send('Armed')
 		state += 1
+
+def alt_in_range(alt, target, eps = 2):
+	return alt >= target-eps and alt <= target+eps
 
 def takeoff():
 	"""
@@ -79,19 +83,31 @@ def takeoff():
 	global cs, cc
 	global state
 
-	msg = cs.recv(BUF_SIZE) # wait for server message
+	msg = json.loads(cs.recv(BUF_SIZE)) # wait for server message
 	
-	if msg != 'TAKEOFF':
+	if msg['msg'] != 'TAKEOFF':
 		error(msg)
 		state = 6 # exit failure
 	else:
-		cc.takeoff(15)
-		# cc.set_mode('GUIDED')
-		# cc.takeoff(json.loads(msg)['alt'])
-		# Insert while loop with timeout here
-		time.sleep(4)
+		cc.takeoff(msg['arg1'])
+
+		to = time.time() + TIMEOUT
+		while not alt_in_range(cc.get_current_location().alt, msg['arg1']):
+			if time.time() > to:
+				cs.send('TIMEOUT')
+				state = 6
+				return
+			time.sleep(0.1)
+		
+		print 'Taken Off to %d' % msg['arg1']
 		cs.send('Taken Off')
 		state += 1
+
+def at_loc((x, y), (cx, cy), eps=.00003):
+	"""
+	check if current lat, lon (x, y) is within eps of target lat, lon (cx, cy)
+	"""
+	return (x - cx)**2 + (y - cy)**2 <= eps**2
 
 def goto_init():
 	"""
@@ -101,29 +117,65 @@ def goto_init():
 
 	print "In the goto_init state"
 
-	global cs
+	global cs, cc
 	global state
 
-	msg = cs.recv(BUF_SIZE) # wait for server message
+	msg = json.loads(cs.recv(BUF_SIZE)) # wait for server message
 	
-	if msg != 'GOTO':
+	if msg['msg'] != 'GOTO':
 		error(msg)
 		state = 6 # exit failure
 	else:
-		# cc.arm()
-		# cc.set_mode('GUIDED')
-		# cc.takeoff(json.loads(msg)['alt'])
-		# Insert while loop with timeout here
-		time.sleep(4)
+		loc = msg['arg1']
+		cc.goto((loc[0], loc[1]), loc[2])
+		cc.set_yaw(250)
+
+		to = time.time() + TIMEOUT
+		while not at_loc((cc.get_current_location().lat, cc.get_current_location().lon), (loc[0], loc[1])):
+			if time.time() > to:
+				cs.send('TIMEOUT')
+				state = 6
+				return
+			time.sleep(0.1)
+
+		print 'Arrived at ', str(loc)
 		cs.send('Arrived')
 		state += 1
 
+def diag_line((lat0, lon0, alt0), (lat, lon, alt), k=5):
+	"""
+	return a path for the drone to follow
+	"""
+	lats = np.linspace(lat0, lat, k)
+	lons = np.linspace(lon0, lon, k)
+	alts = np.linspace(alt0, alt, k)
+	p = zip(lats, lons, alts)
+	return p
+
 def formation():
 	print "Formation"
-	global state
+	global state, cc
 
-	msg = cs.recv(BUF_SIZE)
-	state += 1
+	msg = json.loads(cs.recv(BUF_SIZE))
+	if msg['msg'] != 'FORMATION':
+		error(msg)
+		state = 6
+	else:
+		print 'Starting at ', str(msg['arg1']), ' Going to ', str(msg['arg2']), ' step of ', msg['arg3']
+		ps = diag_line(msg['arg1'], msg['arg2'], msg['arg3'])
+		print 'Path = ', ps
+		for p in ps[1:]:
+			print 'Going to ', p
+			cc.goto((p[0], p[1]), p[2])
+			
+			while not at_loc((cc.get_current_location().lat, cc.get_current_location().lon), (p[0], p[1])):
+				time.sleep(0.01)
+			
+			print 'Arrived'
+			cs.send('Arrived')
+			cs.recv(BUF_SIZE)
+		cs.send('DONE')
+		state += 1
 
 def exit_success():
 	print "EXIT SUCCESS"
@@ -141,8 +193,9 @@ def exit_failure():
 
 PORT = 6060
 BUF_SIZE = 4096
-TIMEOUT = 30 # timeout in seconds
-state = 0 # initial state
+TIMEOUT = 20 # timeout in seconds
+
+state = 0
 cs = None
 
 states = {0 : connect, 1 : arm, 2 : takeoff, 3 : goto_init, 4 : formation, 5 : exit_success, 6 : exit_failure}
