be0607245f695384ba7a657ccfe6f6f89f1193a2
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-a-20809.princeton.edu>
2015-04-19 22:23:29 -0400

    svm stuff

---
+++ ball_tracker/svm/track_ball_svm.py
@@ -0,0 +1,74 @@
+'''
+track_ball_svm.py:
+	Track ball using previously trained 2-class SVM.
+	Create an output video and log.
+Author:
+	Ankush Gola
+'''
+
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.externals import joblib
+import time
+import cv2
+
+def flatten(img):
+	"""
+	Flatten a color image into a vector of pixels
+	"""
+	M, N, P = img.shape
+	return img.reshape(M*N, P)
+
+vw = cv2.VideoWriter
+
+frame_count = 1
+vid = 'cloudy_test.mov'
+out = 'out.avi'
+log = 'ball_track.log'
+
+clf2 = joblib.load('model/clf.pkl')  # read in the model
+f = open(log, 'w') # log file to write to
+
+cap = cv2.VideoCapture('videos/' + vid)
+
+w, h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.cv.CV_CAP_PROP_FPS))
+
+vw = cv2.VideoWriter(out, cv2.cv.FOURCC(*'XVID'), fps, (w, h))
+vw = cv2.VideoWriter(out, -1, fps, (w, h)) # initialize videowriter
+
+while True:
+	_, frame = cap.read()
+	if (frame == None):
+		break
+
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	rgb_f = rgb.astype(float)/255 # scale for classification
+	
+	M, N, P = rgb_f.shape
+	rgb_ff = flatten(rgb_f)
+
+	Z = clf2.predict(rgb_ff)
+	Z = Z.reshape(M, N)
+
+	Z = cv2.GaussianBlur(Z.astype(np.float),(15,15),0)
+	Z = (Z*255).astype(np.uint8)
+
+	circles = cv2.HoughCircles(Z,cv2.cv.CV_HOUGH_GRADIENT,1,1600, param1 = 50, param2 = 10)
+	if circles != None:
+		(x, y, r) = circles[0,:][0]
+		print (x, y, r)
+		cv2.circle(frame, (x, y), r, (0,255,0), 2)
+		cv2.circle(frame,(x, y), 3 ,(0,0,255),3)
+
+	vw.write(frame)
+	cv2.imshow('frame',frame)
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
+
+cap.release()
+vw.close()
+cv2.destroyAllWindows()
---
+++ ball_tracker/svm/train_svm.py
@@ -0,0 +1,49 @@
+'''
+train_svm.py:
+	Train two class SVM for ball tracking.  Save the model as 'model/clf.pkl'
+Author:
+	Ankush Gola
+'''
+
+import numpy as np
+import scipy as sp
+import matplotlib.image as mpimg
+from sklearn import svm
+from sklearn.externals import joblib
+
+def flatten(img):
+	"""
+	Flatten a color image into a vector of pixels
+	"""
+	M, N, P = img.shape
+	return img.reshape(M*N, P)
+
+ball_imgs = ['ball_2', 'ball_3', 'ball_4', 'ball_5', 'ball_6']
+back_imgs = ['sky_1', 'sky_2', 'granite', 'joseph', 'skin', 'skin_2', 'stone', 'sculpt', 'jacket', 'brick']
+
+ball, back = {}, {}
+
+# Flatten images into a single vector
+for img in ball_imgs:
+	ball_img = (mpimg.imread('train/'+img+'.jpg').astype(float))/255
+	train = flatten(ball_img)
+	ball[img] = train
+
+for img in back_imgs:
+	back_img = (mpimg.imread('train/'+img+'.jpg').astype(float))/255
+	train = flatten(back_img)
+	back[img] = train
+
+X_ball, X_back = np.vstack(tuple([ball[k] for k in ball])), np.vstack(tuple([back[k] for k in back]))
+y_ball, y_back = np.ones(X_ball.shape[0]).astype(np.uint8), np.zeros(X_back.shape[0]).astype(np.uint8)
+
+X = np.vstack((X_ball, X_back))
+y = np.hstack((y_ball, y_back))
+
+# Train the SVM
+C = 10
+gamma = 1
+clf = svm.SVC(C=C, kernel='rbf', gamma=gamma, cache_size=1500).fit(X, y)
+
+# Save the model
+joblib.dump(clf, 'model/clf.pkl')
