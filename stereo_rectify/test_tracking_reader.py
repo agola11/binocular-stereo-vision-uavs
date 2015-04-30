44c20350f407d0646d977bcd764caafaeea3f22f
jbolling <josephcbolling@gmail.com>
2015-04-29 20:14:39 -0400

    synchronization fixes, added support for webcam video stabilization

--- stereo_rectify/test_tracking_reader.py
+++ stereo_rectify/test_tracking_reader.py
@@ -1,6 +1,9 @@
 import tracking_reader as tr
 import log_reader as lr
 import numpy as np
+import cv2
+
+vid_fname = "c:\\Users\\Joseph\\Documents\\14-15\\Thesis\\SeniorThesis2015\\ball_tracker\\svm\\videos\\output.mp4"
 
 l_fname = "c:\\Users\\Joseph\Videos\\Flight With Ball\\Left.MP4"
 l_logname = "c:\\Users\\Joseph\Videos\\Flight With Ball\\Left.log"
@@ -17,10 +20,24 @@ r_first_data_time_ms = 31064.366
 C920_data = np.load("C920_calib_data.npz")
 F = C920_data['intrinsic_matrix']
 track_fname = "super_ball_track.log"
-track_offset = 29998   #tracking log is ~29998 ms behind left gopro
+track_offset = 30230   #tracking log is ~29998 ms behind left gopro
 track_data_log_offset = track_offset + stereo_offset
 
 left_reader = lr.LogReader(l_logname,l_first_data_time_ms)
 right_reader = lr.LogReader(r_logname,r_first_data_time_ms)
 
-track = tr.TrackingReader(track_fname,right_reader,track_data_log_offset,F,30,1)
+track = tr.TrackingReader(track_fname,right_reader,track_data_log_offset,F,30,1,vid_fname=vid_fname)
+track.seek_time(r_rect_start_time_ms - track_data_log_offset)
+"""
+orig_window = "Original Video"
+new_window = "Adjusted Video"
+cv2.namedWindow(orig_window,cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow(new_window,cv2.WINDOW_AUTOSIZE)
+
+for i in range(300):
+    rotated_frame, orig_frame = track.get_frame(253)
+    
+    cv2.imshow(orig_window,orig_frame[::2,::2,:])
+    cv2.imshow(new_window,rotated_frame[::2,::2,:])
+    cv2.waitKey(30)
+"""
--- stereo_rectify/tracking_reader.py
+++ stereo_rectify/tracking_reader.py
@@ -1,4 +1,7 @@
 import numpy as np
+import mpl_toolkits.mplot3d as m3d
+import matplotlib.pyplot as plt
+import cv2
 
 # File type constants
 class TrackingReader:
@@ -7,7 +10,7 @@ class TrackingReader:
 
     """
     
-    def __init__(self, fname, log, data_start_time, F, baseline, trans_std):
+    def __init__(self, fname, log, data_start_time, F, baseline, trans_std, vid_fname=None):
         """
         """
         self.fname = fname
@@ -15,6 +18,11 @@ class TrackingReader:
         self.time_ref = data_start_time
         self.F = F
         
+        if vid_fname != None:
+            self.cap = cv2.VideoCapture(vid_fname)
+            assert(self.cap.isOpened())
+            self.w = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+            self.h = int(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
         
         # Read in camera log
         image_positions = self.read_camera_log()
@@ -27,18 +35,67 @@ class TrackingReader:
                                              baseline,
                                              n)
                                              
+        ax = m3d.Axes3D(plt.figure(1))
         for i in range(n):
-            #Translate sigma_points into camera coords
+            # Translate sigma_points into camera coords
             cam_center = vectors[:,i]*baseline
             cam_points = sigma_points[i,:,:] + cam_center[:,None]
-            #Transform into body coords
+            # Rotate into body coords
             body_center = self.camera2body(cam_center)
             body_points = self.camera2body(cam_points)
-            #Transform into NED coords
-            R = self.get_R(i)
-            ned_center = R.dot(body_center)
-            ned_points = R.dot(body_center)
-            print body_points
+            # Rotate into vehicle frame
+            R = self.get_R(self.time[i])
+            Rinv = np.linalg.inv(R)
+            ned_center = Rinv.dot(body_center)
+            ned_points = Rinv.dot(body_points)
+            # Translate into inertial frame
+            T = self.get_T(i)
+            world_center = ned_center + T
+            world_points = ned_points + T[:,None]
+            ax.scatter3D(*T)
+            ax.scatter(*world_center,s = 1,c='r')
+        plt.show()
+    
+    def seek_frame(self, frame):
+        """
+        sets the next frame to be rectified
+        """
+        self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame)
+        
+    def seek_time(self, time):
+        """
+        sets the time from which to select the next frame to be rectified in ms
+        """
+        self.cap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, time)
+        
+    def get_frame(self,target_yaw):
+        """
+        """
+        # Read the next frame
+        retval,frame = self.cap.read()
+        assert(retval)
+        
+        t_adj = self.cap.get(cv2.cv.CV_CAP_PROP_POS_MSEC) + self.time_ref
+    
+        cam2bod = np.array([[1, 0,  0],
+                            [0, 0, -1],
+                            [0, 1,  0]])
+        bod2cam = np.linalg.inv(cam2bod)
+        R1 = self.get_rotation_matrix(self.log.get_ekf_att(t_adj))
+        R1inv = np.linalg.inv(R1)
+        F = self.F
+        Finv = np.linalg.inv(F)
+        #target_att = self.log.get_ekf_att(t_adj)
+        target_att = np.array([0,0,target_yaw])
+        print target_att,"target"
+        R2 = self.get_rotation_matrix(target_att)
+        H = F.dot(bod2cam.dot(R2.dot(R1inv.dot(cam2bod.dot(Finv)))))
+        rotated_frame = cv2.warpPerspective(frame,H,(self.w,self.h))
+        test_point = np.array([500,800,1])
+        print R1
+        print R2
+        print H.dot(test_point)
+        return rotated_frame,frame
         
     def read_camera_log(self):
         """
@@ -61,7 +118,7 @@ class TrackingReader:
             y = np.append(y,float(ystring))
         attfile.close()
         
-        time = time + self.time_ref #Time is in right camera time
+        self.time = time + self.time_ref #Time is in right camera time
         imagepos = np.vstack((x,y,np.ones(y.shape[0])))
         return imagepos
         
@@ -83,10 +140,16 @@ class TrackingReader:
         points = np.tile(point,(num,1,1))
         return points
     
-    def get_R(self,i):
+    def get_R(self,t):
         """
         """
-        att = self.log.get_ekf_att(self.time[i])
+        att = self.log.get_ekf_att(t)
+        print att, "real"
+        R = self.get_rotation_matrix(att)
+        return R
+        
+    def get_rotation_matrix(self, att):
+        
         att = np.deg2rad(att)
         r_pitch = np.array([[ np.cos(att[1]), 0, -np.sin(att[1])],
                             [              0, 1,               0],
@@ -94,11 +157,16 @@ class TrackingReader:
         r_roll = np.array([[1,               0,              0],
                            [0,  np.cos(att[0]), np.sin(att[0])],
                            [0, -np.sin(att[0]), np.cos(att[0])]])
-        r_yaw = np.array([[ np.cos(att[2]), np.sin(att[2]), 0],
+        r_yaw = np.array([[  np.cos(att[2]), np.sin(att[2]), 0],
                           [ -np.sin(att[2]), np.cos(att[2]), 0],
-                          [              0,              0, 1]])
+                          [               0,              0, 1]])
         R = r_roll.dot(r_pitch.dot(r_yaw))
         return R
+        
+    def get_T(self,i):
+        """
+        """
+        return self.log.get_ekf_loc_1d(self.time[i])
         
     def camera2body(self,p):
         """
