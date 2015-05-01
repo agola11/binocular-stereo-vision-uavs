ae2251a416c6715dd765db4632a71b19e555e37c
jbolling <josephcbolling@gmail.com>
2015-04-30 21:28:12 -0400

    Added support for time-based access to means and sigma pointts

--- stereo_rectify/test_tracking_reader.py
+++ stereo_rectify/test_tracking_reader.py
@@ -2,6 +2,8 @@ import tracking_reader as tr
 import log_reader as lr
 import numpy as np
 import cv2
+import mpl_toolkits.mplot3d as m3d
+import matplotlib.pyplot as plt
 
 vid_fname = "c:\\Users\\Joseph\\Documents\\14-15\\Thesis\\SeniorThesis2015\\ball_tracker\\svm\\videos\\output.mp4"
 
@@ -22,12 +24,35 @@ F = C920_data['intrinsic_matrix']
 track_fname = "super_ball_track.log"
 track_offset = 30230   #tracking log is ~29998 ms behind left gopro
 track_data_log_offset = track_offset + stereo_offset
-
+print F
 left_reader = lr.LogReader(l_logname,l_first_data_time_ms)
 right_reader = lr.LogReader(r_logname,r_first_data_time_ms)
 
 track = tr.TrackingReader(track_fname,right_reader,track_data_log_offset,F,30,1,vid_fname=vid_fname)
 track.seek_time(r_rect_start_time_ms - track_data_log_offset)
+
+
+num_points = 100
+times = np.linspace(254008.733, 268241.733, num_points)
+d = np.linspace(20,40,num_points)
+ax = m3d.Axes3D(plt.figure(1))
+for i in range(num_points):
+    observer_loc = right_reader.get_ekf_loc_1d(times[i])
+    ax.scatter3D(*observer_loc)
+    ball_loc = track.get_mean(times[i],d[i])
+    ax.scatter(*ball_loc,c='r')
+
+cov = track.get_cov(times[50],d[50],6)
+ax.scatter(*cov,c='g')
+
+ax.set_xlim3d(-35,10)
+ax.set_ylim3d(-22.5,32.5)
+ax.set_zlim3d(-45,0)
+ax.set_xlabel("North")
+ax.set_ylabel("East")
+ax.set_zlabel("Down")
+plt.show()
+    
 """
 orig_window = "Original Video"
 new_window = "Adjusted Video"
--- stereo_rectify/tracking_reader.py
+++ stereo_rectify/tracking_reader.py
@@ -2,6 +2,7 @@ import numpy as np
 import mpl_toolkits.mplot3d as m3d
 import matplotlib.pyplot as plt
 import cv2
+from scipy import interpolate
 
 # File type constants
 class TrackingReader:
@@ -26,17 +27,19 @@ class TrackingReader:
         
         # Read in camera log
         image_positions = self.read_camera_log()
+        self.pixelx = interpolate.interp1d(self.time, image_positions[0,:])
+        self.pixely = interpolate.interp1d(self.time, image_positions[1,:])
         n = image_positions.shape[1]
         
         # Get vectors towards ball in the right-handed camera frame
-        vectors = self.get_ball_vectors(image_positions)
+        vectors = self.get_ball_vector(image_positions)
         sigma_points = self.get_sigma_points(trans_std,
                                              trans_std,
                                              baseline,
                                              n)
                                              
         ax = m3d.Axes3D(plt.figure(1))
-        for i in range(n):
+        for i in range(0,n):
             # Translate sigma_points into camera coords
             cam_center = vectors[:,i]*baseline
             cam_points = sigma_points[i,:,:] + cam_center[:,None]
@@ -49,13 +52,65 @@ class TrackingReader:
             ned_center = Rinv.dot(body_center)
             ned_points = Rinv.dot(body_points)
             # Translate into inertial frame
-            T = self.get_T(i)
+            T = self.get_T(self.time[i])
             world_center = ned_center + T
             world_points = ned_points + T[:,None]
             ax.scatter3D(*T)
             ax.scatter(*world_center,s = 1,c='r')
         plt.show()
     
+    def get_mean(self, t, d):
+        """
+        Returns the location of the target in the NED world frame assuming distance
+        d
+        """
+        im_pos = np.array([self.pixelx(t),self.pixely(t),1])
+        vec = self.get_ball_vector(im_pos)
+        cam_center = vec*d
+        body_center = self.camera2body(cam_center)
+        # Rotate into vehicle frame
+        R = self.get_R(t)
+        Rinv = np.linalg.inv(R)
+        ned_center = Rinv.dot(body_center)
+        # Translate into inertial frame
+        T = self.get_T(t)
+        inertial_center = ned_center + T
+        return inertial_center
+        
+    def get_cov(self,t,d,sig_perp):
+        """
+        Returns the covariance estimate at time t approximating distance d
+        """
+        sig_x = np.array([sig_perp,0,0])
+        sig_y = np.array([0,sig_perp,0])
+        im_pos = np.array([self.pixelx(t),self.pixely(t),1])
+        perp_points = np.vstack((im_pos-sig_x,
+                                 im_pos + sig_x,
+                                 im_pos - sig_y,
+                                 im_pos + sig_y)).T
+        
+        # Calculate center and sigma points in camera frame
+        vec = self.get_ball_vector(im_pos)
+        cam_center = vec*d
+        cam_sig = self.get_ball_vector(perp_points)*d
+        cam_sig = np.vstack((cam_sig.T, vec*.1*d, vec*1.9*d)).T
+        
+        # Rotate into body frame
+        body_center = self.camera2body(cam_center)
+        body_sig = self.camera2body(cam_sig)
+        
+        # Rotate into vehicle frame
+        R = self.get_R(t)
+        Rinv = np.linalg.inv(R)
+        ned_center = Rinv.dot(body_center)
+        ned_sig = Rinv.dot(body_sig)
+        
+        # Translate into inertial frame
+        T = self.get_T(t)
+        inertial_center = ned_center + T
+        inertial_sig = ned_sig + T[:,None]
+        return inertial_sig
+        
     def seek_frame(self, frame):
         """
         sets the next frame to be rectified
@@ -87,14 +142,9 @@ class TrackingReader:
         Finv = np.linalg.inv(F)
         #target_att = self.log.get_ekf_att(t_adj)
         target_att = np.array([0,0,target_yaw])
-        print target_att,"target"
         R2 = self.get_rotation_matrix(target_att)
         H = F.dot(bod2cam.dot(R2.dot(R1inv.dot(cam2bod.dot(Finv)))))
         rotated_frame = cv2.warpPerspective(frame,H,(self.w,self.h))
-        test_point = np.array([500,800,1])
-        print R1
-        print R2
-        print H.dot(test_point)
         return rotated_frame,frame
         
     def read_camera_log(self):
@@ -122,7 +172,7 @@ class TrackingReader:
         imagepos = np.vstack((x,y,np.ones(y.shape[0])))
         return imagepos
         
-    def get_ball_vectors(self, imagepos):
+    def get_ball_vector(self, imagepos):
         """
         """
         Finv = np.linalg.inv(self.F)
@@ -144,7 +194,6 @@ class TrackingReader:
         """
         """
         att = self.log.get_ekf_att(t)
-        print att, "real"
         R = self.get_rotation_matrix(att)
         return R
         
@@ -163,10 +212,10 @@ class TrackingReader:
         R = r_roll.dot(r_pitch.dot(r_yaw))
         return R
         
-    def get_T(self,i):
+    def get_T(self,t):
         """
         """
-        return self.log.get_ekf_loc_1d(self.time[i])
+        return self.log.get_ekf_loc_1d(t)
         
     def camera2body(self,p):
         """
